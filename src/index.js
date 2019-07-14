c5f5896e9a7a45fbe3eaee8dd26874d4b37a033c
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-07-14 21:45:28 +0200

    Add DevTools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import { Route, Switch } from 'react-router-dom';
 import Navigation from './presentational/navigation.component';
 import Home from './presentational/home.component';
 import Contact from './presentational/contact.component';
 import NotFound from './presentational/not-found.component';
 import "bootstrap";
+import DevTools from './DevTools';
 
 render(
     <Provider store={store}>
+        <div>
         <HashRouter>
             <div>
                 <Route path='/' component={Navigation}>
@@ -23,6 +24,8 @@ render(
                 </Switch>
             </div>
         </HashRouter>
+        <DevTools />
+        </div>
     </Provider>,
     document.getElementById('root')
 );
